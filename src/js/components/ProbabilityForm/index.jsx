9decac3901d0811c4ffa9ee28e5284b052355d93
mrandyvalentine <hello@andyvalentine.co.uk>
2019-02-22 11:38:18 +0000

    Update index.jsx
    Used the morning of my dev day to try refactoring this to use the new 16.8 Hooks instead of a component Class, and it works really nicely.

--- src/js/components/ProbabilityForm/index.jsx
+++ src/js/components/ProbabilityForm/index.jsx
@@ -1,144 +1,130 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ProbabilityFormConstants from './ProbabilityFormConstants';
 import { probabilityCalc, validProbability } from '../../utils/SharedUtils';
 import { Button, Input, Label } from '../Components';
 import './_probabilityform.scss';
 
-class ProbabilityForm extends Component {
-    constructor(props) {
-        super(props);
+const ProbabilityForm = props => {
 
-        // set initial state values
-        this.state = {
-            message: '',
-            previous: [],
-            probability1: '',
-            probability2: '',
-            type: 'combine',
-        }
+    const [message, setMessage] = useState('');
+    const [previous, setPrevious] = useState([]);
+    const [probability1, setProbability1] = useState('');
+    const [probability2, setProbability2] = useState('');
+    const [type, setType] = useState('combine');
+
+    let messageString, previousResultsList, choicesRadio;
+
+    if (message)
+        messageString = <div className="row"><div className={`${ProbabilityFormConstants.BaseClass}__message`}>{message}</div></div>;
+
+    if (previous.length) {
+        previousResultsList = <div className={`${ProbabilityFormConstants.BaseClass}__previous`}>
+            Previous Results
+            <table>
+                <thead><tr><th>P1</th><th>P2</th><th>Type</th><th>Result</th></tr></thead>
+                <tbody>
+                    {previous.map((previous, i) => {
+                        // ideally update this to named object keys
+                        return <tr key={i}><td>{previous[0]}</td><td>{previous[1]}</td><td>{previous[2]}</td><td>{previous[3]}</td></tr>;
+                    })}
+                </tbody>
+            </table>
+        </div>;
     }
 
-    handleSubmit = e => {
-        // form should only be submit with valid values
+    // dynamically create the radio options based on the set constants
+    choicesRadio = Object.keys(ProbabilityFormConstants.Types).map((key) => {
+        return <React.Fragment key={ProbabilityFormConstants.Types[key]} >
+            <input 
+                id={`types-${key}`} 
+                name="types"
+                onChange={ e => setType(key)}
+                type="radio" 
+                value={key}
+                checked={(type === key)} />
+            <Label 
+                for={`types-${key}`}
+                text={ProbabilityFormConstants.Types[key]} />
+            <br/>
+        </React.Fragment>;
+    });
+
+    function handleSubmit(e) {
+    // form should only be submit with valid values
         e.preventDefault();
 
-        const probabilityResult = probabilityCalc(this.state.type, this.state.probability1, this.state.probability2);
-        
+        const probabilityResult = probabilityCalc(type, probability1, probability2);
+            
         // add the newest result to the start of the results list
-        let previousResultsList = this.state.previous;
-        previousResultsList.unshift([this.state.probability1, this.state.probability2, ProbabilityFormConstants.Types[this.state.type], probabilityResult]);
+        let previousResultsList = previous;
+        previousResultsList.unshift([probability1, probability2, ProbabilityFormConstants.Types[type], probabilityResult]);
 
-        // update message and previous results list
-        this.setState({message: `Your result is ${probabilityResult}`, previous: previousResultsList});
-
+        setMessage(`Your result is ${probabilityResult}`);
+        setPrevious(previousResultsList);
     }
 
-    render() {
-        // define the message that is optionally shown under the form
-        let message;
-        if (this.state.message) 
-            message = <div className="row"><div className={`${ProbabilityFormConstants.BaseClass}__message`}>
-                {this.state.message}
-            </div></div>;
-
-        // define the results list that is optionally shown under the form - table for visual data formatting
-        let previousResultsList;
-        if (this.state.previous.length) {
-            previousResultsList = <div className={`${ProbabilityFormConstants.BaseClass}__previous`}>
-                Previous Results
-                <table>
-                    <thead><tr><th>P1</th><th>P2</th><th>Type</th><th>Result</th></tr></thead>
-                    <tbody>
-                        {this.state.previous.map((previous, i) => {
-                            // ideally update this to named object keys
-                            return <tr key={i}><td>{previous[0]}</td><td>{previous[1]}</td><td>{previous[2]}</td><td>{previous[3]}</td></tr>;
-                        })}
-                    </tbody>
-                </table>
-            </div>;
-        }
-
-        // dynamically create the radio options based on the set constants
-        const choicesRadio = Object.keys(ProbabilityFormConstants.Types).map((key) => {
-            return <React.Fragment key={ProbabilityFormConstants.Types[key]} >
-                <input 
-                    id={`types-${key}`} 
-                    name="types"
-                    onChange={ e => this.setState({type : key})}
-                    type="radio" 
-                    value={key}
-                    checked={(this.state.type === key)} />
-                <Label 
-                    for={`types-${key}`}
-                    text={ProbabilityFormConstants.Types[key]} />
-                <br/>
-            </React.Fragment>;
-        }, this);
-
-        return (
-            <React.Fragment>
-                <form 
-                    className={ProbabilityFormConstants.BaseClass}
-                    onSubmit={this.handleSubmit} >
-                    <div className="row">
-                        <Label 
-                            classList={`${ProbabilityFormConstants.BaseClass}__probability-1-label`}
-                            for="probability-1-input"
-                            text="Probability 1"
-                            valid={validProbability(this.state.probability1)}
-                        />
-                        <Input
-                            classList={`${ProbabilityFormConstants.BaseClass}__probability-1-input`}
-                            id="probability-1-input"
-                            max={1}
-                            min={0}
-                            onChange={ e => this.setState( {probability1 : e.target.value} )}
-                            required
-                            type="number"
-                            valid={validProbability(this.state.probability1)}
-                            value={this.state.probability1}
-                        />
-                    </div>
-                    <div className="row">
-                        <Label 
-                            classList={`${ProbabilityFormConstants.BaseClass}__probability-2-label`}
-                            for="probability-2-input"
-                            text="Probability 2"
-                            valid={validProbability(this.state.probability2)}
-                        />
-                        <Input
-                            classList={`${ProbabilityFormConstants.BaseClass}__probability-2-input`}
-                            id="probability-2-input"
-                            max={1}
-                            min={0}
-                            onChange={ e => this.setState( {probability2 : e.target.value} )}
-                            required
-                            type="number"
-                            valid={validProbability(this.state.probability2)}
-                            value={this.state.probability2}
-                        />
-                    </div>
-                    <div className="row">
-                        <ul>
-                            { choicesRadio }
-                        </ul>
-                    </div>
-                    <div className="row">
-                        <Button 
-                            ariaAttributes={{label: "Submit"}}
-                            classList={`${ProbabilityFormConstants.BaseClass}__submit-button`}
-                            // disable the button if both the inputs aren't valid
-                            disabled={!validProbability(this.state.probability1) || !validProbability(this.state.probability2)}
-                            type="submit"
-                        />
-                    </div>
-                </form>
-                {message}
-                {previousResultsList}
-            </React.Fragment>
-        )
-    }
+    return (
+        <React.Fragment>
+            <form 
+                className={ProbabilityFormConstants.BaseClass}
+                onSubmit={handleSubmit} >
+                <div className="row">
+                    <Label 
+                        classList={`${ProbabilityFormConstants.BaseClass}__probability-1-label`}
+                        for="probability-1-input"
+                        text="Probability 1"
+                        valid={validProbability(probability1)}
+                    />
+                    <Input
+                        classList={`${ProbabilityFormConstants.BaseClass}__probability-1-input`}
+                        id="probability-1-input"
+                        max={1}
+                        min={0}
+                        onChange={ e => setProbability1(e.target.value) }
+                        required
+                        type="number"
+                        valid={validProbability(probability1)}
+                        value={probability1}
+                    />
+                </div>
+                <div className="row">
+                    <Label 
+                        classList={`${ProbabilityFormConstants.BaseClass}__probability-2-label`}
+                        for="probability-2-input"
+                        text="Probability 2"
+                        valid={validProbability(probability2)}
+                    />
+                    <Input
+                        classList={`${ProbabilityFormConstants.BaseClass}__probability-2-input`}
+                        id="probability-2-input"
+                        max={1}
+                        min={0}
+                        onChange={ e => setProbability2(e.target.value) }
+                        required
+                        type="number"
+                        valid={validProbability(probability2)}
+                        value={probability2}
+                    />
+                </div>
+                <div className="row">
+                    <ul>
+                        { choicesRadio }
+                    </ul>
+                </div>
+                <div className="row">
+                    <Button 
+                        ariaAttributes={{label: "Submit"}}
+                        classList={`${ProbabilityFormConstants.BaseClass}__submit-button`}
+                        // disable the button if both the inputs aren't valid
+                        disabled={!validProbability(probability1) || !validProbability(probability2)}
+                        type="submit"
+                    />
+                </div>
+            </form>
+            {message}
+            {previousResultsList}
+        </React.Fragment>
+    )
 }
 
 export default ProbabilityForm;
-
